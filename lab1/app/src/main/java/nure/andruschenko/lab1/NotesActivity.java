fb323c8a00dec8c08a2db1a39722caae72c792ce
Stardusted1 <refineanswer@gmail.com>
2020-09-18 21:15:41 +0300

    partially implemented notes functionality

--- lab1/app/src/main/java/nure/andruschenko/lab1/NotesActivity.java
+++ lab1/app/src/main/java/nure/andruschenko/lab1/NotesActivity.java
@@ -1,12 +1,14 @@
 package nure.andruschenko.lab1;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 public class NotesActivity extends AppCompatActivity {
@@ -23,23 +25,28 @@ public class NotesActivity extends AppCompatActivity {
                     return Commons.onOptionsItemSelected(item, NotesActivity.this);
                 }
             });
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("was exception");
         }
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activities_menu, menu);
+        inflater.inflate(R.menu.notes_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.notes_menu).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        System.out.println(item.getItemId());
-        Commons.onOptionsItemSelected(item, NotesActivity.this);
-        return super.onOptionsItemSelected(item);
+    public void onActionCreate(MenuItem item) {
+        System.out.println("action create");
     }
 }
---
+++ lab1/app/src/main/java/nure/andruschenko/lab1/SearchActivity.java
@@ -0,0 +1,26 @@
+package nure.andruschenko.lab1;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SearchActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    private void doMySearch(String query) {
+
+    }
+}
